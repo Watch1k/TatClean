f5b144952542d72ce7bb9aaf3de6788dad684862
Watch1k <host.sf@gmail.com>
2016-04-28 20:11:04 +0300

    commit

--- build/js/app.js
+++ build/js/app.js
@@ -1,6 +1,7 @@
 head.load(
 	"js/lib/jquery.min.js",
-	// "js/lib/slick.min.js",
+	"js/lib/slick.min.js",
+		"js/lib/jquery.countdown.js",
 	// "js/lib/wow.min.js",
 	// "js/lib/owl.carousel.min.js",
 	"js/common.js"
--- src/js/common.js
+++ src/js/common.js
@@ -11,6 +11,49 @@ head.ready(function(){
 		});
 	}());
 
-	// new WOW().init();
+	$('.slider').slick({
+		prevArrow: '<button type="button" class="slick-prev"><i class="icon icon-left"></i></button>',
+		nextArrow: '<button type="button" class="slick-next"><i class="icon icon-right"></i></button>',
+		autoplay: true,
+		autoplaySpeed: 10000
+	});
+
+	$('.slider-com').slick({
+		dots: true,
+		arrows: false,
+		slidesToShow: 3,
+		slideToScroll: 1,
+		centerMode: true,
+		centerPadding: 0,
+		autoplay: true,
+		autoplaySpeed: 10000,
+		focusOnSelect: true
+	});
 	
+	// Counter
+	// Counter
+	(function(){
+		$(window).load(function () {
+			$(".counter__value").each(function(index){
+				var counter = $(this),
+						counterNow = new Date(),
+						counterDate = new Date(2016,6 - 1,25,18,50), // год, месяц (-1 маст хев), день, минута, секунда
+						counterValue = Math.round((counterDate - counterNow) / 1000);
+				counter.countdown({
+					seconds: counterValue,
+					callback:function(days,hours,minutes,seconds,total){
+						days = (days) ? ((days<10)?"0"+days:days)+" : " : "00:";
+						hours = (hours) ? ((hours<10)?"0"+hours:hours)+" : " : "00:";
+						minutes = (minutes) ? ((minutes<10)?"0"+minutes:minutes)+" : " : "00:";
+						seconds = (seconds) ? ((seconds<10)?"0"+seconds:seconds) : "00";
+						counter.html(days+hours+minutes+seconds);
+					},
+					finished: function(){
+						// your code here
+					}
+				});
+			});
+		});
+	}());
+
 });
